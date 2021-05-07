c6c0b9b00dfdf0d989d39d5719a07a8044dfda9c
Aaryan Sharma <60098288+Aaryan8751@users.noreply.github.com>
2021-05-08 04:40:33 +0530

    git commit

--- config.js
+++ config.js
@@ -1,4 +1,4 @@
 module.exports = {
     'secretKey': '12345-67890-09876-54321',
-    'mongoUrl' : 'mongodb://localhost:27017/myhelpDB'
+    'mongoUrl' : 'mongodb+srv://Aaryan:aaryan@cluster0.tnqve.mongodb.net/myhelpDB?retryWrites=true&w=majority'
 } 
