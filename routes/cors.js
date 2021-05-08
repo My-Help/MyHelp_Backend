37d9a91fe07283e3ae77fd1777b9d0799d353754
Aaryan Sharma <60098288+Aaryan8751@users.noreply.github.com>
2021-05-08 06:35:08 +0530

    Update cors.js

--- routes/cors.js
+++ routes/cors.js
@@ -2,7 +2,7 @@ const express = require('express');
 const cors = require('cors');
 const app = express();
 
-const whitelist = ['http://localhost:3000', 'http://localhost:3001'];
+const whitelist = ['https://quirky-pike-20120b.netlify.app'];
 var corsOptionsDelegate = (req, callback) => {
     var corsOptions;
     console.log(req.header('Origin'));
